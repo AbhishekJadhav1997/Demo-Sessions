d56aefea06fb170cc4961990f9ae78ad3b402f7a
mauliA@123 <abhishek.jadhav@nonstopio.com>
2025-11-18 16:38:58 +0530

    New file for loc

---
+++ tests/softAssertions.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('TestAssertions',async({page})=>{
+
+    await page.goto('https://demo.nopcommerce.com/register')
+
+    // 1)expect(page).toHaveURL() — Page has URL
+    await expect(page).toHaveURL('https://demo.nopcommerce.com/register')
+
+    // 2)expect(page).toHaveTitle() — Page has title
+  await expect.soft(page).toHaveTitle('nopCommerce')
+
+  ///
+
+
+
+
+  //3)expect(locator).toBeVisible() — Element is visible
+  const logo=await page.locator('.header-logo')
+  await expect(logo).toBeVisible()
+})
